51c722104d18b81d79f80d6df2530d20e4ff84d2
Chong-U Lim <me@chongdashu.com>
2016-06-30 00:00:28 +0800

    Basic project

---
+++ src/main/scala/chongdashu/conway/ConwayApp.scala
@@ -0,0 +1,12 @@
+package scala.chongdashu.conway
+
+/**
+  * Created by Chong-U Lim on 29 Jun 2016
+  */
+object ConwayApp {
+
+    def main(args : Array[String]) : Unit = {
+        println("Hello, Scala Conway Project!")
+    }
+
+}
